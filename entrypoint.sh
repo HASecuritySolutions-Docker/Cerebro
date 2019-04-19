343115ec9d8a91e77202ea0cf5e76f4c25f84a5f
Justin Henderson <jhenderson@tekrefresh.com>
2019-04-18 21:55:53 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-if [ -z ${var+x} ]; then echo "BASEPATH is unset"; sed -i "s/basePath = \"\/\"/basePath = \"$(echo $BASEPATH | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')\"/g" /opt/cerebro/conf/application.conf; fi
-sed -i "s/http:\/\/elasticsearch:9200/$PROTOCOL:\/\/$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/g" /opt/cerebro/conf/application.conf
-
 cd /opt/cerebro
 /opt/cerebro/bin/cerebro
